f32fd3cfb2d8955f2ed9de01ae8a243c60c59342
Oleksandra Fedorenko <fedorenko.s1989@gmail.com>
2024-11-19 09:40:42 +0200

    appRecord: init

---
+++ script.js
@@ -0,0 +1,6 @@
+const labItems = document.getElementsByClassName('.navigation-item');
+const labList = document.getElementsByClassName('.navigation-list');
+
+document.addEventListener('click', function (e) {
+    console.log(e.target);
+});
