e3927ff794525265b97fd94199a34d5a290e5a99
Oleksandra Fedorenko <fedorenko.s1989@gmail.com>
2024-12-09 13:45:19 +0200

    appRecord: add links content

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const labList = {
     '1.1': {
-        'Опис предметного середовища': 'about.html',
-        'Тема. Мета. Місце розташування сайту та звіту': 'links.html',
-        'Структура докумета': 'str.html',
-        'HTML-код таблиць': 'table.html',
-        'HTML-код списку': 'list.html',
-        'HTML-код зображення': 'img.html',
+        'Опис предметного середовища': './laba_1.1/about.html',
+        'Тема. Мета. Місце розташування сайту та звіту': './laba_1.1/links.html',
+        'Структура докумета': './laba_1.1/str.html',
+        'HTML-код таблиць': './laba_1.1/table.html',
+        'HTML-код списку': './laba_1.1/list.html',
+        'HTML-код зображення': './laba_1.1/img.html',
         'Висновки': ''
     },
     '1.2': {
-        'Тема. Мета. Місце розташування сайту та звіту': 'links.html',
+        'Тема. Мета. Місце розташування сайту та звіту': './laba_1.2/links.html',
         'Способи підключення стилів': '',
         'Селектори': '',
         'Селектор тегу': '',
@@ -36,11 +36,39 @@ let sidebarList = {};
 
 document.querySelector('.navigation-list').addEventListener('click', function (e) {
     addSidebarList(e.target);
+    initFirstSidebarItem();
 });
 
 document.querySelector('.sidebar-list').addEventListener('click', function (e) {
     addContent(e.target);
 });
+
+function addActiveClassForNavigation(element) {
+    const navigationItems = document.querySelectorAll('.navigation-item');
+    navigationItems.forEach((elem) => elem.classList.remove('active'));
+    if (Array.from(navigationItems).find((item) => item === element)) {
+        element.classList.add('active');
+    }
+}
+
+function addActiveClassForSidebar(element) {
+    const sidebarItems = document.querySelectorAll('.sidebar-item');
+    sidebarItems.forEach((elem) => elem.classList.remove('active'));
+    
+    if (Array.from(sidebarItems).find((item) => item === element)) {
+        element.classList.add('active');
+    }
+}
+
+function initFirstNavigationItem() {
+    const navigationItem = document.querySelector('.navigation-item');
+    addSidebarList(navigationItem);
+}
+
+function initFirstSidebarItem() {
+    const sidebarItem = document.querySelector('.sidebar-item');
+    addContent(sidebarItem);
+}
 
 function addSidebarList(element) {
     const sidebar = document.querySelector('.sidebar-list');
@@ -49,11 +77,17 @@ function addSidebarList(element) {
     Object.keys(sidebarList).forEach(element => {
         let liItem = document.createElement('li');
         liItem.innerHTML = element;
+        liItem.classList.add('sidebar-item');
         sidebar.appendChild(liItem);
     });
+    addActiveClassForNavigation(element);
 }
 
 function addContent(element) {
     const content = document.querySelector('.content_lab');
-    content.innerHTML = `<iframe style="width: 100%; height: 100%;" src="./laba_1.1/${sidebarList[element.innerHTML]}" frameborder="0">`;
+    content.innerHTML = `<iframe style="width: 100%; height: 100%;" src="${sidebarList[element.innerHTML]}" frameborder="0">`;
+    addActiveClassForSidebar(element);
 }
+
+initFirstNavigationItem();
+initFirstSidebarItem();
